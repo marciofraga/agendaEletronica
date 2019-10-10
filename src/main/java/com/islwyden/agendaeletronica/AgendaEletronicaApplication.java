015b26ef3b6144647954cad40eb82acfb69a0590
marcio fraga <marcior.mfraga@gmail.com>
2019-10-10 12:00:29 -0300

    projeto criado

---
+++ src/main/java/com/islwyden/agendaeletronica/AgendaEletronicaApplication.java
@@ -0,0 +1,13 @@
+package com.islwyden.agendaeletronica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaEletronicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaEletronicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/islwyden/agendaeletronica/AgendaEletronicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.islwyden.agendaeletronica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaEletronicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
