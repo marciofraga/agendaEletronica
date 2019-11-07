aae51ea1ac8c0d5ecf5fa862fe2af8321c040085
Marcio Fraga <marcior.mfraga@gmail.com>
2019-11-07 16:38:41 -0300

    CRUD JQuery para consumir a API banckend

--- src/main/java/com/islwyden/agendaeletronica/controller/ContatoController.java
+++ src/main/java/com/islwyden/agendaeletronica/controller/ContatoController.java
@@ -22,35 +22,35 @@ public class ContatoController {
 	@Autowired
 	private ContatoService service;
 	
-	@RequestMapping(method = RequestMethod.GET)
+	@RequestMapping(method = RequestMethod.GET, produces = "application/json")
 	public ResponseEntity<List<Contato>> listar() {
 		List<Contato> contatos = service.buscarTodos();
 		return ResponseEntity.ok().body(contatos);
 	}
 	
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = "application/json")
 	public ResponseEntity<Contato> buscar(@PathVariable Long id) {
 		Contato obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
 	}
 	
-	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<Void> inserir(@RequestBody Contato obj) {
+	@RequestMapping(method = RequestMethod.POST, produces = "application/json")
+	public ResponseEntity<Contato> inserir(@RequestBody Contato obj) {
 		obj = service.inserir(obj);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		//URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		
-		return ResponseEntity.created(uri).build();
+		return ResponseEntity.ok().body(obj);
 	}
 	
-	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-	public ResponseEntity<Void> atualizar(@RequestBody Contato obj, 
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT, produces = "application/json")
+	public ResponseEntity<Contato> atualizar(@RequestBody Contato obj, 
 											@PathVariable Long id) {
 		obj.setId(id);
 		service.atualizar(obj);
-		return ResponseEntity.noContent().build();
+		return ResponseEntity.ok().body(obj);
 	}
 	
-	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = "application/json")
 	public ResponseEntity<Void> excluir(@PathVariable Long id) {
 		service.excluir(id);
 		return ResponseEntity.noContent().build();
--- src/main/resources/static/js/agenda.js
+++ src/main/resources/static/js/agenda.js
@@ -1,34 +1,120 @@
-$(document).ready(function() {
-	listarTodos();
-	consultaContato(1); // 1 é o identificador do contato que deseja ser consultado
-});
+// variável que representa a linha selecionada
+var $dt;
 
-
-function listarTodos() {
-	$.get( "http://localhost:8080/contatos", function(data){
-		
-		var contatos = data;
-		data.forEach(function(contato, indice) {
-			console.log(data);
-			adicionar(data);
-		});
-		
-	});	
-}
-
-function consultaContato(id) {
-	$.get( "http://localhost:8080/contatos/" + id, function(data){
-		console.log(data);
-		adicionar(data);
-	});	
-}
-
+//função para criar uma nova linha
 function adicionar(contato) {
 	var linha = $('#linha dt:last-child').clone();
 		
 	$('.nome', linha).html(contato.nome);
 	$('.telefone', linha).html(contato.telefone);
 	$('.email', linha).html(contato.email);
-	
+	$('.excluir', linha).attr('data-id', contato.id).attr('hidden', false).html('Excluir');
+	$('.selecionar', linha).attr('hidden', false).html('Selecionar');
 	$('#linha').append(linha);
 };
+
+// JQuery CRUD para o recurso contato
+$(document).ready(function() {
+	// realiza a listagem de todos os contatos
+	$.ajax({
+		type:'GET',
+		url: '/contatos',
+		success: function(contatos) {
+			contatos.forEach(function(contato, indice) {
+				console.log(contato);
+				adicionar(contato);
+			});
+		},
+		error: function() {
+			alert("erro ao listar contatos");
+		}
+	});
+	
+	// realiza o cadastro de um contato
+	$('#enviar').on('click', function() {
+		var contato = {
+			nome: $('#nome').val(),
+			telefone: $('#telefone').val(),
+			email: $('#email').val()
+		};
+		
+		$.ajax({
+			method: 'POST',
+			url: '/contatos',
+			contentType: 'application/json',
+			data: JSON.stringify(contato),
+			success: function(contato) {
+				console.log(contato);
+				adicionar(contato);
+			},
+			error: function() {
+				console.log(contato);
+				alert('erro ao cadastrar contato');
+			}
+		});
+	});
+	
+	// realiza a exclusão de um contato
+	$('#lista').delegate('.excluir', 'click', function() {
+		
+		$dt = $(this).closest('dt');
+		
+		$.ajax({
+			method: 'DELETE',
+			url: '/contatos/' + $(this).attr('data-id'),
+			success: function() {
+				$dt.fadeOut(200, function() {
+					$(this).remove();
+				});
+			},
+			error: function() {
+				alert("Erro ao excluir contato");
+			}
+		});
+	});
+	
+	// realiza a seleção de um contato a ser alterado
+	$('#lista').delegate('.selecionar', 'click', function() {
+		
+		$dt = $(this).closest('dt');
+		
+		$('#nome').val($dt.find('span.nome').html());
+		$('#telefone').val($dt.find('span.telefone').html());
+		$('#email').val($dt.find('span.email').html());
+		$('#editar').attr('data-id', $dt.find('button.excluir').attr('data-id'));
+	});
+	
+	//envia a requisição para alterar um contato em específico
+	$('#editar').on('click', function() {
+		var $editar = $(this).closest('button');
+		
+		var contato = {
+				nome: $('#nome').val(),
+				telefone: $('#telefone').val(),
+				email: $('#email').val()
+			};
+		
+		$.ajax({
+			method: 'PUT',
+			url: '/contatos/' + $(this).attr('data-id'),
+			contentType: 'application/json',
+			data: JSON.stringify(contato), 
+			success: function(contato) {
+				
+				$dt.find('span.nome').html(contato.nome);
+				$dt.find('span.telefone').html(contato.telefone);
+				$dt.find('span.email').html(contato.email);
+				$dt.find('button.excluir').attr('data-id', contato.id);
+				
+				$('#nome').val("");
+				$('#telefone').val("");
+				$('#email').val("");
+				$('#editar').attr("");
+				
+			},
+			error: function() {
+				alert("Erro ao alterar contato");
+			}
+	});
+	});
+});
